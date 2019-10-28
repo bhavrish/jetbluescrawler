15e33dfe03551ba90faa13cca66e3614049877ef
Bhavesh Shah <bhavrish@gmail.com>
2019-10-28 14:57:08 -0400

    backend fixes

--- jetBlueWebApp/home/views.py
+++ jetBlueWebApp/home/views.py
@@ -8,18 +8,21 @@ def home(request):
 	return render(request,'index.html')
 
 def availabilitybad(request):
-	availableData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/availability.json').read()
+	availableData = open('./staticfiles/availability.json').read()
 	availableTweets = json.loads(availableData)
 
 	learn = load_learner(".")
 
 	for availableTweet in availableTweets:
 		availableTweet["result"]=learn.predict(availableTweet["tweet"])[0]
+
+		if (str(availableTweet["result"])=="neutral" or str(availableTweet["result"])=="positive"):
+			availableTweets.remove(availableTweet)
 
 	return render(request,'availabilitybad.html', context={"availableTweets":availableTweets})
 
 def availabilitygood(request):
-	availableData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/availability.json').read()
+	availableData = open('./staticfiles/availability.json').read()
 	availableTweets = json.loads(availableData)
 
 	learn = load_learner(".")
@@ -27,21 +30,27 @@ def availabilitygood(request):
 	for availableTweet in availableTweets:
 		availableTweet["result"]=learn.predict(availableTweet["tweet"])[0]
 
+		if (str(availableTweet["result"])=="negative"):
+			availableTweets.remove(availableTweet)
+
 	return render(request,'availabilitygood.html', context={"availableTweets":availableTweets})
 
 def costbad(request):
-	costData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/cost.json').read()
+	costData = open('./staticfiles/cost.json').read()
 	costTweets = json.loads(costData)
 
 	learn = load_learner(".")
 
 	for costTweet in costTweets:
 		costTweet["result"]=learn.predict(costTweet["tweet"])[0]
+
+		if (str(costTweet["result"])=="neutral" or str(costTweet["result"])=="positive"):
+			costTweets.remove(costTweet)
 
 	return render(request,'costbad.html', context={"costTweets":costTweets})
 
 def costgood(request):
-	costData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/cost.json').read()
+	costData = open('./staticfiles/cost.json').read()
 	costTweets = json.loads(costData)
 
 	learn = load_learner(".")
@@ -49,21 +58,27 @@ def costgood(request):
 	for costTweet in costTweets:
 		costTweet["result"]=learn.predict(costTweet["tweet"])[0]
 
+		if (str(costTweet["result"])=="negative"):
+			costTweets.remove(costTweet)
+
 	return render(request,'costgood.html', context={"costTweets":costTweets})
 
 def legroombad(request):
-	legroomData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/legroom.json').read()
+	legroomData = open('./staticfiles/legroom.json').read()
 	legroomTweets = json.loads(legroomData)
 
 	learn = load_learner(".")
 
 	for legroomTweet in legroomTweets:
 		legroomTweet["result"]=learn.predict(legroomTweet["tweet"])[0]
+
+		if (str(legroomTweet["result"])=="neutral" or str(legroomTweet["result"])=="positive"):
+			legroomTweets.remove(legroomTweet)
 
 	return render(request,'legroombad.html', context={"legroomTweets":legroomTweets})
 
 def legroomgood(request):
-	legroomData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/legroom.json').read()
+	legroomData = open('./staticfiles/legroom.json').read()
 	legroomTweets = json.loads(legroomData)
 
 	learn = load_learner(".")
@@ -71,26 +86,35 @@ def legroomgood(request):
 	for legroomTweet in legroomTweets:
 		legroomTweet["result"]=learn.predict(legroomTweet["tweet"])[0]
 
+		if (str(legroomTweet["result"])=="negative"):
+			legroomTweets.remove(legroomTweet)
+
 	return render(request,'legroomgood.html', context={"legroomTweets":legroomTweets})
 
 def timelinessbad(request):
-	timeData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/time.json').read()
+	timeData = open('./staticfiles/time.json').read()
 	timeTweets = json.loads(timeData)
 
 	learn = load_learner(".")
 
 	for timeTweet in timeTweets:
 		timeTweet["result"]=learn.predict(timeTweet["tweet"])[0]
+
+		if (str(timeTweet["result"])=="neutral" or str(timeTweet["result"])=="positive"):
+			timeTweets.remove(timeTweet)
 
 	return render(request,'timelinessbad.html', context={"timeTweets":timeTweets})
 
 def timelinessgood(request):
-	timeData = open('/Users/bhaveshshah/Desktop/jetblue/jetBlueWebApp/staticfiles/time.json').read()
+	timeData = open('./staticfiles/time.json').read()
 	timeTweets = json.loads(timeData)
 
 	learn = load_learner(".")
 
 	for timeTweet in timeTweets:
 		timeTweet["result"]=learn.predict(timeTweet["tweet"])[0]
+
+		if (str(timeTweet["result"])=="negative"):
+			timeTweets.remove(timeTweet)
 
 	return render(request,'timelinessgood.html', context={"timeTweets":timeTweets})
