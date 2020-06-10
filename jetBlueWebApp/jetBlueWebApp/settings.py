b37c3e219f230f5e5c6241e633d0f7002fdc78ef
bhavrish <bhavrish@gmail.com>
2020-06-10 15:59:06 -0400

    clean up repo

--- jetBlueWebApp/jetBlueWebApp/settings.py
+++ jetBlueWebApp/jetBlueWebApp/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
-    'tweets.apps.TweetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
