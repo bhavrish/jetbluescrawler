ad610ee93ff53ae09383f7fc175c27b5e9c0e81c
bhavrish <bhavrish@gmail.com>
2020-06-14 00:51:24 -0400

    Resolve static bugs issue

--- jetBlueWebApp/settings.py
+++ jetBlueWebApp/settings.py
@@ -128,12 +128,12 @@ MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 django_heroku.settings(locals())
